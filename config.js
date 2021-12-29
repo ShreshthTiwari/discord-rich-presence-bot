baf9b6e9ec77dbc2e710b8ee70226bab063420a1
ShreshthTiwari <shreshth.shreya24208@gmail.com>
2021-12-29 22:28:37 +0530

    updates

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  CLIENT_ID: '', // Discord bot application's cLient ID.
+  FIRST_LINE: 'Playing With Nukes!',  //First line of the RPC.
+  SECOND_LINE: 'O Ma Gawd. WaaoooW!', //Second line of the RPC.
+  IMAGE_TEXT: 'NUKES!', //Text shown when sombody hovers over the image.
+  IMAGE_NAME: 'omigawd', //Name of the image that you uploaded in rich presence, assets.
+  BUTTON_1_URL: 'https://youtu.be/dQw4w9WgXcQ', //URL that will open when somebody presses the first button.
+  BUTTON_1_TEXT: 'Don\'t Press', //Text written on the first button.
+  BUTTON_2_URL: 'https://youtu.be/0EC7MODYQe8', //URL that will open when somebody presses the second button.
+  BUTTON_2_TEXT: 'HELP', //Text written on the second button.
+  TOKEN: '' // Discord bot token.
+}
